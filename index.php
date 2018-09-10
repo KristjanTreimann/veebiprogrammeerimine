2f1e84f83448917c247ed8d82da8de5cbdccac38
Kristjan Treimann <KristjanTreimann@users.noreply.github.com>
2018-09-10 10:40:52 +0300

    html muudetud php-ks

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Kristjan Treimann, õppetöö</title>
+</head>
+<body>
+	<h1>Kristjan Treimann, IF18</h1>
+	<p>Nothing to see here. Move on...</p>
+	<p>Lisasin siia paar rida. Vaata ka <a href="http://www.tlu.ee" target="_blank">TLÜ</a> kodulehte.</p>
+	 <!--img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">-->
+	 <img src="../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">
+	<p>Mul on sõber, kes teeb ka <a href="../../~henrtam"> veebi </a>.</p>
+
+
+</body>
+</html>	
